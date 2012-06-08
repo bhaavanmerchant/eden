62336bf40c7021c81230130cb9a9fabc11d2a833
Bhaavan Merchant <bhaavanmerchant@gmail.com>
2012-06-08 17:18:24 -0500

    Made generation of table dynamic. Some data now parsed from the controller.

--- controllers/roster.py
+++ controllers/roster.py
@@ -9,8 +9,8 @@
 module = request.controller
 resourcename = request.function
 
-if not deployment_settings.has_module(module):
-    raise HTTP(404, body="Module disabled: %s" % module)
+#if not deployment_settings.has_module(module):
+#    raise HTTP(404, body="Module disabled: %s" % module)
 
 # -----------------------------------------------------------------------------
 def index():
@@ -18,3 +18,5 @@ def index():
         Application Home page
         - custom View
     """
+
+    return dict(message=T("Rostering Tool"), numb=6, projects=['Project Alpha', 'Project Beta', 'Peoject Gamma', 'Project Delta', 'Project Epsilon', 'Project Zeta', 'Project Eta'],slots=['8:00 - 12:00','12:00 - 4:00','4:00 - 8:00'],job_roles=['-- Select --','Team Leader', 'Team Member', 'Trainee'])
