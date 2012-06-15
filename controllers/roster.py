c0ad1c84454746639d9b821b497cbf4c7f2212c9
Bhaavan Merchant <bhaavanmerchant@gmail.com>
2012-06-16 05:06:29 +0530

    Added a volunteer column, submission event on select of new job role.

--- controllers/roster.py
+++ controllers/roster.py
@@ -19,4 +19,9 @@ def index():
         - custom View
     """
     alloted_roles=['Team Leader', 'Team Member', 'Team Member', 'Team Member', 'Trainee', 'Trainee', 'Trainee' ]
-    return dict(message=T("Rostering Tool"), numb=6, projects=['Project Alpha', 'Project Beta', 'Peoject Gamma', 'Project Delta', 'Project Epsilon', 'Project Zeta', 'Project Eta'],slots=['8:00 - 12:00','12:00 - 4:00','4:00 - 8:00'],job_roles=['-- Select --','Team Leader', 'Team Member', 'Trainee'], alloted_roles=alloted_roles)
+    volunteers=['Mari Hargis', 'Ismael Nolin','Sherry Febres','Barabara Gamino','Augustina Northam','Artie Timms','Kimberely Lamey','Ignacio Crumble','Vinnie Launius','Roxane Cremin'];
+    return dict(message=T("Rostering Tool"), numb=6, projects=['Project Alpha', 'Project Beta', 'Peoject Gamma', 'Project Delta', 'Project Epsilon', 'Project Zeta', 'Project Eta'],slots=['8:00 - 12:00','12:00 - 4:00','4:00 - 8:00'],job_roles=['-- Select --','Team Leader', 'Team Member', 'Trainee'], alloted_roles=alloted_roles,volunteers=volunteers)
+
+def add_role():
+    alloted_roles=['Team Leader']
+    return dict(alloted_roles=alloted_roles)
