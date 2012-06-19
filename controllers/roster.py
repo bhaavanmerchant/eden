e029a49866711bd93b43307136c71ea951331f87
Bhaavan Merchant <bhaavanmerchant@gmail.com>
2012-06-20 00:15:02 +0530

    Added the minus feature and many class changes, shifted vid to be stored in data.

--- controllers/roster.py
+++ controllers/roster.py
@@ -20,4 +20,5 @@ def index():
     """
     alloted_roles=['Team Leader', 'Team Member', 'Team Member', 'Team Member', 'Trainee', 'Trainee', 'Trainee' ]
     volunteers={'a1':'Mari Hargis', 'a2':'Ismael Nolin','a3':'Sherry Febres','a4':'Barabara Gamino','a5':'Augustina Northam','a6':'Artie Timms','a7':'Kimberely Lamey','a8':'Ignacio Crumble','a9':'Vinnie Launius','a10':'Roxane Cremin'}; # {volunteer_id:volunteer_name}
-    return dict(message=T("Rostering Tool"), numb=6, projects=['Project Alpha', 'Project Beta', 'Peoject Gamma', 'Project Delta', 'Project Epsilon', 'Project Zeta', 'Project Eta'],slots=['8:00 - 12:00','12:00 - 4:00','4:00 - 8:00'],job_roles=['-- Select --','Team Leader', 'Team Member', 'Trainee'], alloted_roles=alloted_roles,volunteers=volunteers)
+    time_dets=['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday'];
+    return dict(message=T("Rostering Tool"), numb=6, projects=['Project Alpha', 'Project Beta', 'Peoject Gamma', 'Project Delta', 'Project Epsilon', 'Project Zeta', 'Project Eta'],slots=['8:00 - 12:00','12:00 - 4:00','4:00 - 8:00'],job_roles=['-- Select --','Team Leader', 'Team Member', 'Trainee'], alloted_roles=alloted_roles,volunteers=volunteers,time_dets=time_dets)
