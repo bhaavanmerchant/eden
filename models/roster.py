df8d8487d3d9c24927afbca868897a9f33d2dd83
Bhaavan Merchant <bhaavanmerchant@gmail.com>
2012-07-15 01:13:11 +0530

    Replaced old definition

--- models/roster.py
+++ models/roster.py
@@ -6,23 +6,16 @@ table = db.define_table(tablename, Field('week'), Field('slot'))
 tablename = "hrm_roster"
 table = db.define_table(tablename, Field('roster_table',db.hrm_roster_table), *s3_meta_fields())
 tablename = "hrm_roster_organisation"
-table = db.define_table(tablename, Field('roster',db.hrm_roster), s3db.super_link("org_id", "org_organisation"))
+table = db.define_table(tablename, Field('roster',db.hrm_roster), s3db.super_link("id", "org_organisation"))
 tablename = "hrm_roster_project"
-table = db.define_table(tablename, Field('roster',db.hrm_roster), s3db.super_link("project_id", "project_project"))
+table = db.define_table(tablename, Field('roster',db.hrm_roster), s3db.super_link("id", "project_project"))
 tablename = "hrm_roster_site"
 table = db.define_table(tablename, Field('roster',db.hrm_roster),  s3db.super_link("site_id", "org_site"))
 tablename = "hrm_roster_incident" 
-table = db.define_table(tablename, Field('roster',db.hrm_roster), s3db.super_link("event_id", "event_incident"))
+table = db.define_table(tablename, Field('roster',db.hrm_roster), s3db.super_link("id", "event_incident"))
 tablename = "hrm_roster_scenario"
-table = db.define_table(tablename, Field('roster',db.hrm_roster), s3db.super_link("scenario_id", "scenario_scenario"))
+table = db.define_table(tablename, Field('roster',db.hrm_roster), s3db.super_link("id", "scenario_scenario"))
 tablename = "hrm_roster_shift"
-#table = db.define_table(tablename, 
-#                            Field('roster',db.hrm_roster),
-#                            s3db.project_task_id(label=T("task")), 
-#                            s3db.pr_person_id(label=T("person")), 
-#                            Field("starttime", "datetime", label=T("start")), 
-#                            Field("endtime", "datetime", label=T("end"))
-#                        )
 table = db.define_table(tablename,
                             Field("roster_id",db.hrm_roster),
                             Field("table_id",db.hrm_roster_table),
