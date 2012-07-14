e3d5b4da18929fdfb98421bc0e57947890a5fd2e
Bhaavan Merchant <bhaavanmerchant@gmail.com>
2012-07-15 01:58:56 +0530

    Changed to normal link instead of a super link due to non creation of a corresponding linkable field in the inheriting table

--- models/roster.py
+++ models/roster.py
@@ -2,19 +2,44 @@
 #tablename = "hrm_roster_slots"
 #table = db.define_table(tablename, Field('event'), Field('event_id'), Field('slot'))
 tablename = "hrm_roster_table"
-table = db.define_table(tablename, Field('week'), Field('slot'))
+table = db.define_table(tablename, 
+                                Field('week'), 
+                                Field('slot')
+                        )
 tablename = "hrm_roster"
-table = db.define_table(tablename, Field('roster_table',db.hrm_roster_table), *s3_meta_fields())
+table = db.define_table(tablename, 
+                                Field('roster_table',
+                                    db.hrm_roster_table
+                        ), *s3_meta_fields())
 tablename = "hrm_roster_organisation"
-table = db.define_table(tablename, Field('roster',db.hrm_roster), s3db.super_link("id", "org_organisation"))
+table = db.define_table(tablename, 
+                                Field('roster',db.hrm_roster), 
+                                #s3db.super_link("id", "org_organisation")
+                                s3db.org_organisation_id()
+                        )
 tablename = "hrm_roster_project"
-table = db.define_table(tablename, Field('roster',db.hrm_roster), s3db.super_link("id", "project_project"))
+table = db.define_table(tablename, 
+                                Field('roster',db.hrm_roster), 
+                                #s3db.super_link("id", "project_project")
+                                s3db.project_project_id()
+                        )
 tablename = "hrm_roster_site"
-table = db.define_table(tablename, Field('roster',db.hrm_roster),  s3db.super_link("site_id", "org_site"))
+table = db.define_table(tablename, 
+                                Field('roster',db.hrm_roster), 
+                                s3db.super_link("site_id", "org_site")
+                        )
 tablename = "hrm_roster_incident" 
-table = db.define_table(tablename, Field('roster',db.hrm_roster), s3db.super_link("id", "event_incident"))
+table = db.define_table(tablename, 
+                                Field('roster',db.hrm_roster), 
+                                #s3db.super_link("id", "event_incident")
+                                s3db.event_incident_id()
+                        )
 tablename = "hrm_roster_scenario"
-table = db.define_table(tablename, Field('roster',db.hrm_roster), s3db.super_link("id", "scenario_scenario"))
+table = db.define_table(tablename, 
+                                Field('roster',db.hrm_roster), 
+                                #s3db.super_link("id", "scenario_scenario")
+                                s3db.scenario_scenario_id()
+                        )
 tablename = "hrm_roster_shift"
 table = db.define_table(tablename,
                             Field("roster_id",db.hrm_roster),
