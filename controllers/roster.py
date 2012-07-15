f05ac8aac4a3f057e26e7b665324de974e716508
Bhaavan Merchant <bhaavanmerchant@gmail.com>
2012-07-15 22:47:34 +0530

    Shifted to a more formal initialization of variable in roster/index to acomodate database input

--- controllers/roster.py
+++ controllers/roster.py
@@ -23,7 +23,10 @@ def index():
     volunteers={'a1':'Mari Hargis', 'a2':'Ismael Nolin','a3':'Sherry Febres','a4':'Barabara Gamino','a5':'Augustina Northam','a6':'Artie Timms','a7':'Kimberely Lamey','a8':'Ignacio Crumble','a9':'Vinnie Launius','a10':'Roxane Cremin'}; # {volunteer_id:volunteer_name}
     time_dets=['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday'];
     project_date=datetime.date(2012,4,4);
-    return dict(message=T("Rostering Tool"), numb=6, projects=['Project Alpha', 'Project Beta', 'Peoject Gamma', 'Project Delta', 'Project Epsilon', 'Project Zeta', 'Project Eta'],slots=['8:00 - 12:00','12:00 - 4:00','4:00 - 8:00'],job_roles=['-- Select --','Team Leader', 'Team Member', 'Trainee'], alloted_roles=alloted_roles,volunteers=volunteers,time_dets=time_dets,project_date=project_date)
+    slots=['8:00 - 12:00','12:00 - 4:00','4:00 - 8:00']
+    job_roles=['-- Select --','Team Leader', 'Team Member', 'Trainee']
+    projects=['Project Alpha', 'Project Beta', 'Peoject Gamma', 'Project Delta', 'Project Epsilon', 'Project Zeta', 'Project Eta']
+    return dict(message=T("Rostering Tool"), numb=6, projects=projects,slots=slots,job_roles=job_roles, alloted_roles=alloted_roles,volunteers=volunteers,time_dets=time_dets,project_date=project_date)
 
 def people():
     """
