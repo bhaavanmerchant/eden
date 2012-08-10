4747b19dca8af048794215a6b5d20a0a19dbfcfb
Bhaavan Merchant <bhaavanmerchant@gmail.com>
2012-08-10 21:25:22 +0530

    Cleared off unwanted tables.

--- models/roster.py
+++ models/roster.py
@@ -1,6 +1,4 @@
 ## -*- coding: utf-8 -*-
-##tablename = "hrm_roster_slots"
-##table = db.define_table(tablename, Field('event'), Field('event_id'), Field('slot'))
 tablename = "hrm_roster_table"
 table = db.define_table(tablename, 
                                 Field('occasion'),
@@ -8,11 +6,7 @@ table = db.define_table(tablename,
                                 Field('slot'),
                                 s3db.project_project_id()
                         )
-##tablename = "hrm_roster_event"
-##table = db.define_table(tablename,
-##                                Field('occasion'),
-##                
-##                        )
+
 ##tablename = "hrm_roster_date"
 ##table = db.define_table(tablename,
 ##                            Field('type'),
@@ -28,35 +22,6 @@ table = db.define_table(tablename,
                                  Field('change_req'),
                                  *s3_meta_fields()
                         )
-##tablename = "hrm_roster_organisation"
-##table = db.define_table(tablename, 
-##                                Field('roster',db.hrm_roster), 
-##                                #s3db.super_link("id", "org_organisation")
-##                                s3db.org_organisation_id()
-##                        )
-##tablename = "hrm_roster_project"
-##table = db.define_table(tablename, 
-##                                Field('roster',db.hrm_roster), 
-##                                #s3db.super_link("id", "project_project")
-##                                s3db.project_project_id()
-##                        )
-##tablename = "hrm_roster_site"
-##table = db.define_table(tablename, 
-##                                Field('roster',db.hrm_roster), 
-##                                s3db.super_link("site_id", "org_site")
-##                        )
-##tablename = "hrm_roster_incident" 
-##table = db.define_table(tablename, 
-##                                Field('roster',db.hrm_roster), 
-##                                #s3db.super_link("id", "event_incident")
-##                                s3db.irs_ireport_id()
-##                        )
-##tablename = "hrm_roster_scenario"
-##table = db.define_table(tablename, 
-##                                Field('roster',db.hrm_roster), 
-##                                #s3db.super_link("id", "scenario_scenario")
-##                                s3db.scenario_scenario_id()
-##                        )
 tablename = "hrm_roster_shift"
 table = db.define_table(tablename,
                             Field("roster_id",db.hrm_roster),
@@ -65,14 +30,11 @@ table = db.define_table(tablename,
                             Field("role"),
                             s3db.pr_person_id()
                         )
-
 tablename = "hrm_roster_roles" #roles: volunteer, team leader etc. defined for a table.
 table = db.define_table(tablename, Field("table_id", db.hrm_roster_table), Field('roles'), Field('position_in_table','integer'))
 
 tablename = "hrm_person_role" #Clarification needed if the naming of this is apt or should be changed ?
 table = db.define_table(tablename, s3db.hrm_job_role_id(), s3db.pr_person_id())
-
-
 
 #tablename = "hrm_roster_change"
 #table = db.define_table(tablename,
